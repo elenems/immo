116d81e30910f04934cca9322c12970694329593
Ostap Brukh <ostapbrukh@gmail.com>
2020-02-09 12:28:36 +0200

    Add links switch based on auth status

--- shared/Header/HeaderButtons/index.jsx
+++ shared/Header/HeaderButtons/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Router from 'next/router';
+import { connect } from 'react-redux';
 import CustomLink from '../../../ui_components/CustomLink/index';
 import Button from '../../../ui_components/Button/index';
 
-export default function HeaderButtons() {
+const redirectTo = (page) => {
+  Router.push(page);
+};
+
+function HeaderButtons({ isAuthenticated }) {
+  const page = isAuthenticated ? '/profile' : '/login';
   return (
     <div>
       <div className="header-controls">
-        <Button text="Load" title="Load your photo" callback={() => {}} />
-        <CustomLink href="/join" text="Join" title="Join Immo" />
+        <Button text="Load" title="Load your photo" callback={() => redirectTo(page)} />
+        {isAuthenticated ? <CustomLink href="/logout" text="Logout" title="Logout" /> : <CustomLink href="/join" text="Join" title="Join Immo" />}
       </div>
       <style jsx>
         {`
@@ -15,10 +23,18 @@ export default function HeaderButtons() {
             display: flex;
             align-items: center;
             justify-content: space-between;
-            min-width: 125px;
+            min-width: 135px;
           }
         `}
       </style>
     </div>
   );
 }
+
+HeaderButtons.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = (state) => ({ isAuthenticated: state.auth.isAuthenticated });
+
+export default connect(mapStateToProps)(HeaderButtons);
