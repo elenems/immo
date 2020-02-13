0224c614ee74a4d6cbe3966a2f2bc7aa61f041a8
elenems <mickhaelyarish@gmail.com>
2020-02-13 12:53:21 +0200

    Add views and like button.

--- components/PhotoPage/PhotoContainer/index.jsx
+++ components/PhotoPage/PhotoContainer/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import UserInfo from '../../../shared/UserInfo/index';
 import PhotoDescription from '../../../shared/PhotoDescription/index';
+import ViewsDisplay from '../../../ui_components/ViewsDisplay/index';
+import HeartButton from '../../../ui_components/HeartButton/index';
 
 function PhotoContainer({
   photoUrl,
@@ -10,13 +12,21 @@ function PhotoContainer({
   lastName,
   email,
   tags,
+  views,
+  photoId,
 }) {
   return (
-    <div>
-      <img src={photoUrl} alt={name} />
+    <div className="photo-container">
+      <div className="photo">
+        <img src={photoUrl} alt={name} />
+      </div>
       <div className="block">
         <PhotoDescription name={name} tags={tags} />
         <UserInfo firstName={firstName} lastName={lastName} email={email} />
+      </div>
+      <div className="block">
+        <ViewsDisplay views={views} />
+        <HeartButton id={photoId} color="#000" />
       </div>
       <style jsx>
         {`
@@ -24,7 +34,10 @@ function PhotoContainer({
             display: flex;
             justify-content: space-between;
           }
-          div {
+          .block:last-child {
+            border-top: 2px solid #d81b60;
+          }
+          .photo-container {
             max-width: 430px;
             min-width: 200px;
             width: 100%;
@@ -49,6 +62,8 @@ PhotoContainer.propTypes = {
   lastName: PropTypes.string,
   tags: PropTypes.array.isRequired,
   email: PropTypes.string.isRequired,
+  views: PropTypes.number.isRequired,
+  photoId: PropTypes.string.isRequired,
 };
 
 PhotoContainer.defaultProps = {
