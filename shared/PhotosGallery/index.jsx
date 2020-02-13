91e6903c90c83c260ef59031a403f2d953026a1b
elenems <mickhaelyarish@gmail.com>
2020-02-13 12:55:19 +0200

    Add configutable image width.

--- shared/PhotosGallery/index.jsx
+++ shared/PhotosGallery/index.jsx
@@ -3,18 +3,31 @@ import PropTypes from 'prop-types';
 import Photo from './Photo/index';
 import './index.scss';
 
-function PhotosGallery({ photos }) {
+function PhotosGallery({ photos, imageWidth }) {
   return (
     <div className="photos-gallery">
-      {photos.map((photo) => {
-        return <Photo key={photo.id} photo={photo} />;
-      })}
+      {photos.map((photo) => <Photo key={photo.id} photo={photo} />)}
+      <style jsx>
+        {`
+          div {
+            grid-template-columns: repeat(
+              auto-fill,
+              minmax(${imageWidth}px, 1fr)
+            );
+          }
+        `}
+      </style>
     </div>
   );
 }
 
+PhotosGallery.defaultProps = {
+  imageWidth: 320,
+};
+
 PhotosGallery.propTypes = {
   photos: PropTypes.array.isRequired,
+  imageWidth: PropTypes.number,
 };
 
 export default PhotosGallery;
