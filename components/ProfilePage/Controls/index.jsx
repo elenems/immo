6c804702bcb02723628f2b80225269ca17ea15e4
elenems <mickhaelyarish@gmail.com>
2020-02-14 14:06:47 +0200

    Add user's credentials and links.

--- components/ProfilePage/Controls/index.jsx
+++ components/ProfilePage/Controls/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import './index.scss';
 import CustomLink from '../../../ui_components/CustomLink/index';
 
-export default function Controls() {
+function Controls({ firstName, lastName, loading }) {
   return (
     <div className="profile-controls">
+      <div className="user-greeting">
+        <span>{!loading ? `Hello ${firstName} ${lastName}` : null}</span>
+      </div>
       <div>
         <CustomLink
-          text="Load"
+          text="Load photo"
           color="#d81b60"
-          title="Load"
-          href="/Load"
+          title="Load photo"
+          href="/load"
         />
       </div>
       <div>
@@ -18,7 +23,7 @@ export default function Controls() {
           text="Logout"
           color="#d81b60"
           title="Logout"
-          href="/load"
+          href="/logout"
         />
       </div>
       <style jsx>
@@ -26,14 +31,39 @@ export default function Controls() {
           .profile-controls {
             display: flex;
             flex-direction: column;
-            width: 200px;
+            width: 15%;
+            border-right: 1px solid #d81b60;
           }
 
           .profile-controls > div {
             margin-bottom: 12px;
+          }
+
+          .profile-controls .user-greeting {
+            margin-bottom: 30px;
+            font-size: 18px;
+            font-weight: 500;
           }
         `}
       </style>
     </div>
   );
 }
+
+Controls.propTypes = {
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+};
+
+Controls.defaultProps = {
+  lastName: '',
+  firstName: '',
+};
+
+const mapStateToProps = (state) => ({
+  firstName: state.auth.user.firstName,
+  lastName: state.auth.user.lastName,
+});
+
+export default connect(mapStateToProps)(Controls);
