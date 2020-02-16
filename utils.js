ce7a5d0b75218ea0e9d9378fc90492d19bab9f1c
elenems <mickhaelyarish@gmail.com>
2020-02-16 14:00:24 +0200

    Add redirects depends on auth status.

--- utils.js
+++ utils.js
@@ -1,8 +1,46 @@
+import jwtDecode from 'jwt-decode';
+import Router from 'next/router';
+
 export const dispatchShowCard = (show, hide, text, type, isInProcess) => {
   if (!isInProcess) {
     show({ text, type });
     setTimeout(() => {
       hide();
     }, 3500);
+  }
+};
+
+export const guardAuthPage = () => {
+  if (process.browser) {
+    // eslint-disable-next-line no-undef
+    const token = sessionStorage.getItem('token');
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        if (decodedToken.exp * 1000 < Date.now()) {
+          Router.push('/join');
+        }
+      } catch (e) {
+        Router.push('/join');
+      }
+    } else {
+      Router.push('/join');
+    }
+  }
+};
+
+export const redirectFromAuthPage = () => {
+  if (process.browser) {
+    // eslint-disable-next-line no-undef
+    const token = sessionStorage.getItem('token');
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        if (decodedToken.exp * 1000 > Date.now()) {
+          Router.push('/profile');
+        }
+      // eslint-disable-next-line no-empty
+      } catch (e) {}
+    }
   }
 };
